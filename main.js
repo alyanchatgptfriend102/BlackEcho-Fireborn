7366c44310ee7319f7e7ac027568d3b680681425
alyanchatgptfriend102 <fourfriends021@gmail.com>
2025-06-23 21:13:40 +0500

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
 function startGame() {
-    window.location.href = "game.html";
+    alert("Loading game... please wait!");
+
+    document.body.innerHTML = `
+        <div style="color: white; text-align: center; padding-top: 50px;">
+            <h1>Game Started!</h1>
+            <p>This is where your game will run.</p>
+        </div>
+    `;
 }
